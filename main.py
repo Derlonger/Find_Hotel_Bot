b3750e5c9039dab19c2b8137b6c605c44a036a63
Derlonger <o.uskova29@mail.ru>
2023-06-13 11:09:39 +0300

    Создал класс для реализации состояние пользователя внутри сценария.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from loader import bot, logger
+from loader import bot
 from utils.set_bot_commands import set_default_commands
+import handlers  # noqa
+from telebot.custom_filters import StateFilter, IsDigitFilter
 
 
-@logger.catch
 def run_bot():
     set_default_commands(bot)
     bot.infinity_polling()
+    bot.add_custom_filter(StateFilter(bot))
+    bot.add_custom_filter(IsDigitFilter())
 
 
 if __name__ == "__main__":
