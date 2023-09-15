e45c3068fdaa53fa63f3012df3a2a446658fd2ee
Derlonger <o.uskova29@mail.ru>
2023-09-15 13:34:36 +0300

    убрал LSTEP

--- handlers/callback_handlers/select_date.py
+++ handlers/callback_handlers/select_date.py
@@ -1,7 +1,7 @@
 import utils.show_data_and_find_hotels
 from loader import bot
-from telebot.types import CallbackQuery, ReplyKeyboardRemove
-from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
+from telebot.types import CallbackQuery
+from telegram_bot_calendar import DetailedTelegramCalendar
 from datetime import date, timedelta
 from loguru import logger
 from states.user_states import UserInputState
@@ -13,7 +13,7 @@ def send_start_calendar(callback: CallbackQuery) -> None:
     result, key, step = DetailedTelegramCalendar(calendar_id=1, min_date=date.today(), locale='ru').process(
         callback.data)
     if not result and key:
-        bot.edit_message_text(f'Выберите {LSTEP[step]} заезда',
+        bot.edit_message_text(f'Выберите дату заезда:',
                               callback.message.chat.id,
                               callback.message.message_id,
                               reply_markup=key)
@@ -24,7 +24,7 @@ def send_start_calendar(callback: CallbackQuery) -> None:
                               callback.message.chat.id,
                               callback.message.message_id, reply_markup=None)
         calendar, step = DetailedTelegramCalendar(calendar_id=2, min_date=date.today(), locale='ru').build()
-        bot.send_message(callback.message.chat.id, f"Выберите {LSTEP[step]} выезда",
+        bot.send_message(callback.message.chat.id, f"Выберите дату выезда:",
                          reply_markup=calendar)
         bot.delete_message(callback.message.chat.id, callback.message.message_id)
 
@@ -37,7 +37,7 @@ def send_end_calendar(callback: CallbackQuery) -> None:
         result, key, step = DetailedTelegramCalendar(calendar_id=2, min_date=new_start_date, locale='ru').process(
             callback.data)
         if not result and key:
-            bot.edit_message_text(f"Выберите {LSTEP[step]} выезда",
+            bot.edit_message_text(f"Выберите дату выезда",
                                   callback.message.chat.id,
                                   callback.message.message_id,
                                   reply_markup=key)
